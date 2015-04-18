f4c6096e817d4fd6d277e7fbbf87b79eaa0b2c8c
Carsten Bleicker <carsten@bleicker.de>
2015-04-18 11:33:47 +0200

    Introduce Fuild

--- src/WebApplication.php
+++ src/WebApplication.php
@@ -12,6 +12,8 @@ use Bleicker\Response\MainResponseInterface;
 use Bleicker\Routing\RouterInterface;
 use Bleicker\View\ViewResolver;
 use Bleicker\View\ViewResolverInterface;
+use TYPO3\Fluid\Core\Cache\FluidCacheInterface;
+use TYPO3\Fluid\Core\Cache\SimpleFileCache;
 
 /**
  * Class WebApplication
@@ -26,7 +28,8 @@ class WebApplication extends AbstractKernel implements ApplicationInterface {
 		static::getRegistry()->addImplementation(MainResponseInterface::class, new Response());
 		static::getRegistry()->addImplementation(RouterInterface::class, Router::getInstance());
 		static::getRegistry()->addImplementation(ViewResolverInterface::class, new ViewResolver());
-		static::getRegistry()->addImplementation(HandlerInterface::class, new Handler(static::getRegistry()->getImplementation(MainRequestInterface::class), static::getRegistry()->getImplementation(MainResponseInterface::class)));
+		static::getRegistry()->addImplementation(HandlerInterface::class, new Handler());
+		static::getRegistry()->addImplementation(FluidCacheInterface::class, new SimpleFileCache(ROOT_DIRECTORY . '/Cache'));
 	}
 
 	/**
@@ -35,7 +38,7 @@ class WebApplication extends AbstractKernel implements ApplicationInterface {
 	public function run() {
 		/** @var Handler $httpHandler */
 		$httpHandler = static::getRegistry()->getImplementation(HandlerInterface::class);
-		$httpHandler->handle();
+		$httpHandler->initialize()->handle();
 
 		/** @var Response $response */
 		$response = static::getRegistry()->getImplementation(MainResponseInterface::class);
