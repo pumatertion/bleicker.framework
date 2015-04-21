0b71bee6c6b856357effe634abb0352646353ea8
Carsten Bleicker <carsten@bleicker.de>
2015-04-21 15:24:58 +0200

    Enable Routing Cache if Context is Production

--- src/WebApplication.php
+++ src/WebApplication.php
@@ -23,7 +23,7 @@ class WebApplication extends AbstractKernel implements ApplicationInterface {
 		parent::__construct();
 		Registry::addImplementation(MainRequestInterface::class, Request::createFromGlobals());
 		Registry::addImplementation(MainResponseInterface::class, new Response());
-		Registry::addImplementation(RouterInterface::class, Router::getInstance(ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'Cache' . DIRECTORY_SEPARATOR . 'route.cache.php', Context::isDevelopment() ? FALSE : TRUE));
+		Registry::addImplementation(RouterInterface::class, Router::getInstance(ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'Cache' . DIRECTORY_SEPARATOR . 'route.cache.php', Context::isProduction() ? FALSE : TRUE));
 		Registry::addImplementation(HandlerInterface::class, new Handler());
 	}
 
