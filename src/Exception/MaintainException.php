8ba46edd063126b707dd710946a96f14dc5e1c02
Carsten Bleicker <carsten@bleicker.de>
2015-04-23 16:10:48 +0200

    Introduce MaintainException

---
+++ src/Exception/MaintainException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bleicker\Framework\Exception;
+
+use Bleicker\Exception\ThrowableExceptionInterface;
+use Bleicker\Security\Exception\AbstractVoterException as Exception;
+
+/**
+ * Class MaintainException
+ *
+ * @package Bleicker\Framework\Exception
+ */
+class MaintainException extends Exception implements ThrowableExceptionInterface {
+
+}
