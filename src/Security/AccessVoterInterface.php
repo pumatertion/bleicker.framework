70405aed284fd4b52228f118d35a68d614ea87fc
Carsten Bleicker <carsten@bleicker.de>
2015-04-23 15:32:04 +0200

    Introduce missing AccessVoterInterface::addVote() method

--- src/Security/AccessVoterInterface.php
+++ src/Security/AccessVoterInterface.php
@@ -4,6 +4,7 @@ namespace Bleicker\Framework\Security;
 
 use Bleicker\Security\Exception\AbstractVoterException;
 use Bleicker\Security\Exception\InvalidVoterExceptionException;
+use Bleicker\Security\VoteInterface;
 use Closure;
 
 /**
@@ -12,6 +13,12 @@ use Closure;
  * @package Bleicker\Framework\Security
  */
 interface AccessVoterInterface {
+
+	/**
+	 * @param VoteInterface $vote
+	 * @return $this
+	 */
+	public function addVote(VoteInterface $vote);
 
 	/**
 	 * @param string $for
