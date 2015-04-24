1070ade0f47876891d3e3a53ce8b7b47e496c648
Carsten Bleicker <carsten@bleicker.de>
2015-04-24 13:26:37 +0200

    Remove AbstractRegistry

--- src/Registry.php
+++ src/Registry.php
@@ -7,6 +7,44 @@ namespace Bleicker\Framework;
  *
  * @package Bleicker\Framework
  */
-class Registry extends AbstractRegistry {
+class Registry implements RegistryInterface {
 
+	/**
+	 * @var array
+	 */
+	public static $storage = [];
+
+	/**
+	 * @param string $path
+	 * @param mixed|null $value
+	 * @return void
+	 */
+	public static function add($path, $value = NULL) {
+		static::$storage[$path] = $value;
+	}
+
+	/**
+	 * @param string $path
+	 * @return mixed
+	 */
+	public static function get($path) {
+		if (array_key_exists($path, static::$storage)) {
+			return static::$storage[$path];
+		}
+		return NULL;
+	}
+
+	/**
+	 * @return array
+	 */
+	public static function getAll() {
+		return static::$storage;
+	}
+
+	/**
+	 * @return void
+	 */
+	public static function prune() {
+		static::$storage = [];
+	}
 }
