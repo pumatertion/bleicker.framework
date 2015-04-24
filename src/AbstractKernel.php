b713e07d5538799dfcdabc5fd8dd4e82be0b09a3
Carsten Bleicker <carsten@bleicker.de>
2015-04-24 14:52:05 +0200

    Register System TypeConverter

--- src/AbstractKernel.php
+++ src/AbstractKernel.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace Bleicker\Framework;
+use Bleicker\Framework\Object\Converter;
+use Bleicker\Framework\Object\TypeConverter\FloatTypeConverter;
+use Bleicker\Framework\Object\TypeConverter\IntegerTypeConverter;
+use Bleicker\Framework\Object\TypeConverter\StringTypeConverter;
 
 /**
  * Class Kernel
@@ -9,4 +13,9 @@ namespace Bleicker\Framework;
  */
 abstract class AbstractKernel implements KernelInterface {
 
+	public function __construct() {
+		Converter::register(IntegerTypeConverter::class, new IntegerTypeConverter());
+		Converter::register(FloatTypeConverter::class, new FloatTypeConverter());
+		Converter::register(StringTypeConverter::class, new StringTypeConverter());
+	}
 }
