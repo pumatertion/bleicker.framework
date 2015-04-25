8e883674ed4e22daa2bb67af98e5502be37dcd63
Carsten Bleicker <carsten@bleicker.de>
2015-04-25 20:48:05 +0200

    Webapplication registers authentication requirements

--- src/WebApplication.php
+++ src/WebApplication.php
@@ -2,6 +2,8 @@
 
 namespace Bleicker\Framework;
 
+use Bleicker\Authentication\AuthenticationManager;
+use Bleicker\Authentication\AuthenticationManagerInterface;
 use Bleicker\FastRouter\Router;
 use Bleicker\Framework\Context\Context;
 use Bleicker\Framework\Http\Handler;
@@ -14,6 +16,10 @@ use Bleicker\Request\MainRequestInterface;
 use Bleicker\Response\Http\Response;
 use Bleicker\Response\MainResponseInterface;
 use Bleicker\Routing\RouterInterface;
+use Bleicker\Token\TokenManager;
+use Bleicker\Token\TokenManagerInterface;
+use Bleicker\Session\SessionInterface;
+use Bleicker\Session\Session;
 
 /**
  * Class WebApplication
@@ -24,8 +30,13 @@ class WebApplication extends AbstractKernel implements ApplicationInterface {
 
 	public function __construct() {
 		parent::__construct();
-		ObjectManager::register(MainRequestInterface::class, Request::createFromGlobals());
+		ObjectManager::register(SessionInterface::class, new Session());
+		$request = Request::createFromGlobals();
+		$request->setSession(ObjectManager::get(SessionInterface::class));
+		ObjectManager::register(MainRequestInterface::class, $request);
 		ObjectManager::register(MainResponseInterface::class, new Response());
+		ObjectManager::register(TokenManagerInterface::class, new TokenManager());
+		ObjectManager::register(AuthenticationManagerInterface::class, new AuthenticationManager());
 		ObjectManager::register(RouterInterface::class, Router::getInstance(ROOT_DIRECTORY . DIRECTORY_SEPARATOR . 'Cache' . DIRECTORY_SEPARATOR . 'route.cache.php', Context::isProduction() ? FALSE : TRUE));
 		ObjectManager::register(AccessVoterInterface::class, new AccessVoter());
 		ObjectManager::register(HandlerInterface::class, new Handler());
