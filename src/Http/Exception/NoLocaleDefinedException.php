3412dec0e9d441393a0fd6030d485b04f7385580
Carsten Bleicker <carsten@bleicker.de>
2015-05-07 22:53:34 +0200

    Http Handler sets system locale

---
+++ src/Http/Exception/NoLocaleDefinedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bleicker\Framework\Http\Exception;
+
+use Bleicker\Exception\ThrowableException as Exception;
+
+/**
+ * Class NoLocaleDefinedException
+ *
+ * @package Bleicker\Framework\Http\Exception
+ */
+class NoLocaleDefinedException extends Exception {
+
+}
---
+++ src/Http/Exception/RequestedLocaleNotDefinedException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bleicker\Framework\Http\Exception;
+
+use Bleicker\Exception\ThrowableException as Exception;
+
+/**
+ * Class RequestedLocaleNotDefinedException
+ *
+ * @package Bleicker\Framework\Http\Exception
+ */
+class RequestedLocaleNotDefinedException extends Exception {
+
+}
