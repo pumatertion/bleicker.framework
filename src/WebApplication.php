941ca4a4f1aca4739ef0ffca93c3e72a38c13005
Carsten Bleicker <carsten@bleicker.de>
2015-05-09 22:18:02 +0200

    Using new Objectmanager in Webapplication

--- src/WebApplication.php
+++ src/WebApplication.php
@@ -2,24 +2,16 @@
 
 namespace Bleicker\Framework;
 
-use Bleicker\Authentication\AuthenticationManager;
-use Bleicker\Authentication\AuthenticationManagerInterface;
-use Bleicker\FastRouter\Router;
-use Bleicker\Framework\Context\Context;
 use Bleicker\Framework\Http\Handler;
-use Bleicker\Framework\Http\Request;
-use Bleicker\Framework\Security\AccessVoter;
-use Bleicker\Framework\Security\AccessVoterInterface;
+use Bleicker\Framework\Http\RequestFactory;
 use Bleicker\ObjectManager\ObjectManager;
 use Bleicker\Request\HandlerInterface;
 use Bleicker\Request\MainRequestInterface;
 use Bleicker\Response\Http\Response;
 use Bleicker\Response\MainResponseInterface;
-use Bleicker\Routing\RouterInterface;
-use Bleicker\Token\TokenManager;
-use Bleicker\Token\TokenManagerInterface;
-use Bleicker\Session\SessionInterface;
 use Bleicker\Session\Session;
+use Bleicker\Session\SessionInterface;
+use Bleicker\Framework\Http\RequestInterface;
 
 /**
  * Class WebApplication
@@ -32,12 +24,23 @@ class WebApplication extends AbstractKernel implements ApplicationInterface {
 	 * @return void
 	 */
 	public function run() {
-		/** @var Handler $httpHandler */
-		$httpHandler = ObjectManager::get(HandlerInterface::class);
+		/** @var HandlerInterface $httpHandler */
+		$httpHandler = ObjectManager::get(HandlerInterface::class, Handler::class);
 		$httpHandler->initialize()->handle();
 
-		/** @var Response $response */
-		$response = ObjectManager::get(MainResponseInterface::class);
+		/** @var RequestInterface $httpRequest */
+		$httpRequest = ObjectManager::get(MainRequestInterface::class, function () {
+			$request = RequestFactory::getInstance();
+			ObjectManager::add(MainRequestInterface::class, $request);
+			return $request;
+		});
+
+		/** @var SessionInterface $session */
+		$session = ObjectManager::get(SessionInterface::class, Session::class);
+		$httpRequest->setSession($session);
+
+		/** @var MainResponseInterface $response */
+		$response = ObjectManager::get(MainResponseInterface::class, Response::class);
 		$response->send();
 	}
 }
