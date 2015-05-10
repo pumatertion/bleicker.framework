eb72950b981b2107d063977914da08af70a144d4
Carsten Bleicker <carsten@bleicker.de>
2015-05-10 21:29:13 +0200

    Introduce AuthenticationRequiredException

---
+++ src/Security/Vote/Exception/AuthenticationRequiredException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bleicker\Framework\Security\Vote\Exception;
+
+use Bleicker\Security\Exception\AbstractVoteException;
+
+/**
+ * Class AuthenticationRequiredException
+ *
+ * @package Bleicker\Framework\Security\Vote\Exception
+ */
+class AuthenticationRequiredException extends AbstractVoteException {
+
+}
