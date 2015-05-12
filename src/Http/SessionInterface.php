9ef7089326e5644d93b17c2e1de84353b7b41297
Carsten Bleicker <carsten@bleicker.de>
2015-05-12 12:39:09 +0200

    Introduce Http/SessionInterface

---
+++ src/Http/SessionInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bleicker\Framework\Http;
+
+use Symfony\Component\HttpFoundation\Session\SessionInterface as SessionInterfaceOrigin;
+
+/**
+ * Interface SessionInterface
+ *
+ * @package Bleicker\Framework\Http
+ */
+interface SessionInterface extends SessionInterfaceOrigin {
+
+}
