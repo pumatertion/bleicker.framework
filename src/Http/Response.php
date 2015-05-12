c9e5e9e33872c5b66bf0099f6b5d3b9e5b0fc1e6
Carsten Bleicker <carsten@bleicker.de>
2015-05-12 11:04:17 +0200

    Introduce Http/Response

---
+++ src/Http/Response.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Bleicker\Framework\Http;
+
+use Bleicker\Response\MainResponseInterface;
+use Bleicker\Response\ResponseInterface;
+use Symfony\Component\HttpFoundation\Response as HttpResponse;
+
+/**
+ * Class Response
+ *
+ * @package Bleicker\Framework\Http
+ */
+class Response extends HttpResponse implements MainResponseInterface {
+
+	/**
+	 * @var ResponseInterface
+	 */
+	protected $parentResponse;
+
+	/**
+	 * @return ResponseInterface
+	 */
+	public function getParentResponse() {
+		return $this->parentResponse;
+	}
+
+	/**
+	 * @param ResponseInterface $parentResponse
+	 * @return $this
+	 */
+	public function setParentResponse(ResponseInterface $parentResponse) {
+		$this->parentResponse = $parentResponse;
+		return $this;
+	}
+
+	/**
+	 * @return ResponseInterface
+	 */
+	public function getMainResponse() {
+		$parentResponse = $this->getParentResponse();
+		if ($parentResponse === NULL) {
+			return $this;
+		}
+		if ($parentResponse->getParentResponse() instanceof ResponseInterface) {
+			return $parentResponse->getParentResponse();
+		}
+		return $parentResponse;
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function isMainResponse() {
+		return $this->getMainResponse() === $this;
+	}
+}
